80ec577e472a842096d55545f5ec99ca84554328
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-01-25 10:01:42 -0300

    Criei os partials header e heade

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -7,6 +7,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -23,9 +24,11 @@ def contact(request, contact_id):
 
     # O django tem um atalho para o  código acima
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True) # Também é possível fazer um filter
+    contact_name = f'{single_contact.first_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': contact_name
     }
 
     return render(
