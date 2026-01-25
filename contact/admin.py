49847a34bc3167d21b4ba0e82aa2422343eef2d0
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-01-25 09:29:57 -0300

    Criei o template,a view e a url para exibir o contato único

--- contact/admin.py
+++ contact/admin.py
@@ -5,13 +5,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone',)
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'show',)
     ordering = ('id',) # Colocando um -id altera a ordem para decrescente
     # list_filter = ('created_date',)
     search_fields = ('id', 'first_name', 'last_name',)
     list_per_page = (15)
     list_max_show_all = (200)
-    # list_editable = ('first_name', 'last_name',) Posso editar diretamente pela aba inicial do admin, sem precisar entrar no contato
+    list_editable = ('first_name', 'last_name', 'show',) #Posso editar diretamente pela aba inicial do admin, sem precisar entrar no contato
     list_display_links = ('id', 'phone',)
 
 @admin.register(models.Category)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
 
     context = {
         'contacts': contacts,
@@ -11,5 +12,24 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context
+    )
+
+def contact(request, contact_id):
+    # Esse é o método 'convencional' de mostrar uma página não existente sem erro de index, apenas o erro404
+    # single_contact = Contact.objects.get(pk=contact_id)
+    # if single_contact is None:
+    #     raise Http404
+
+    # O django tem um atalho para o  código acima
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True) # Também é possível fazer um filter
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context
     )
