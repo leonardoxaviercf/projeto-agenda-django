1587906033de76ef0135c0b8a62bf21478f1565d
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-01-25 13:08:09 -0300

    Criação do create.html e form do create

--- contact/urls.py
+++ contact/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     # padrao para CRUD
     path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
-    # path('contact/create/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
     # path('contact/<int:contact_id>/update/', views.contact, name='contact'),
     # path('contact/<int:contact_id>/delete/', views.contact, name='contact'),
 ]
