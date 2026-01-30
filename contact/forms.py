757d6e749b0101ac460b0a10594d3d31955c02e0
leonardoxaviercf <leonardocruzfilho8@gmail.com>
2026-01-30 16:22:20 -0300

    Configurando widgets e help text

--- contact/forms.py
+++ contact/forms.py
@@ -3,6 +3,23 @@ from django.core.exceptions import ValidationError
 from . import models
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder':'Seu nome'
+            }
+        ),
+        label='Primeiro nome',
+        help_text='Texto de ajuda',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'placeholder': 'Seu nome'
+        # })
+
     class Meta:
         model = models.Contact
         fields = (
@@ -10,6 +27,14 @@ class ContactForm(forms.ModelForm):
             'last_name',
             'phone',
         )
+
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs= {
+        #             'placeholder': 'Seu nome'
+        #         }
+        #     )
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
